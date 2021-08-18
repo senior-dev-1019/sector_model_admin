552f25a8d3ff022a0225d88244fe0b8d53eab22d
Senior Dev <fightingmonkey1@gmail.com>
2021-08-18 12:06:23 +0900

    add filter option

--- sector_app/views.py
+++ sector_app/views.py
@@ -7,5 +7,5 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def Listviewdisplaydata(request):
-    results = Ratings.objects.all()
+    results = Ratings.objects.filter( rating_debt__debt_org__org_sector__sector_name__contains="Automotive" )
     return render(request, "Index.html", {'Ratings': results})
