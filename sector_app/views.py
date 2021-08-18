95641dcffd7bae6c35e60c7f0aa1e738a12c2b7d
Senior Dev <fightingmonkey1@gmail.com>
2021-08-18 12:14:31 +0900

    add multi filter option

--- sector_app/views.py
+++ sector_app/views.py
@@ -7,5 +7,5 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def Listviewdisplaydata(request):
-    results = Ratings.objects.filter( rating_debt__debt_org__org_sector__sector_name__contains="Automotive" )
+    results = Ratings.objects.filter( rating_debt__debt_org__org_sector__sector_name__contains="Automotive").filter(rating_debt__debt_org__org_name__contains="Range Rover" ).filter(rating_debt__debt_name__contains="debt_name_2")
     return render(request, "Index.html", {'Ratings': results})
