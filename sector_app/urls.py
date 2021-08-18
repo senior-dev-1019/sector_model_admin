3cdb5d0ed49ef70b7b2157aee6c1f1c7b52aa95a
Senior Dev <fightingmonkey1@gmail.com>
2021-08-18 14:15:12 +0900

    generate filter script by org and sector

--- sector_app/urls.py
+++ sector_app/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('listviewdisplaydata', views.Listviewdisplaydata),
+    path('listviewdisplaydata', views.listviewdisplaydata),
+    path('orgdashboard', views.orgdashboard),
+    path('sectororgs/<int:sector_id>', views.sectororogs),
+    path('sectororgs/<int:sector_id>/<str:org_name>', views.sectororogdebts),
 ]
--- sector_app/views.py
+++ sector_app/views.py
@@ -6,6 +6,22 @@ from django.utils import timezone
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def Listviewdisplaydata(request):
-    results = Ratings.objects.filter( rating_debt__debt_org__org_sector__sector_name__contains="Automotive").filter(rating_debt__debt_org__org_name__contains="Range Rover" ).filter(rating_debt__debt_name__contains="debt_name_2")
+def listviewdisplaydata(request):
+    results = Ratings.objects.filter( rating_debt__debt_org__org_sector__sector_name__contains="Automotive") \
+    .filter(rating_debt__debt_org__org_name__contains="Range Rover" ) \
+    .filter(rating_debt__debt_name__contains="debt_name_2")
+    return render(request, "Index.html", {'Ratings': results})
+
+def orgdashboard(request):
+    results = Sector.objects.all();
+    return render(request, "orgdashboard.html", {'Sectors': results})
+
+def sectororogs(request, sector_id):
+    results = Ratings.objects.filter( rating_debt__debt_org__org_sector__id=sector_id )
+    return render(request, "Index.html", {'Ratings': results})
+
+def sectororogdebts(request, sector_id, org_name):
+    print(org_name)
+    results = Ratings.objects.filter( rating_debt__debt_org__org_sector__id=sector_id ) \
+    .filter( rating_debt__debt_org__org_name__contains=org_name )
     return render(request, "Index.html", {'Ratings': results})
