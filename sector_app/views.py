82e8c9fc955e7b523b452bc5d4adc53b46223e88
senior-developer1019 <romo1112@yandex.com>
2021-08-25 05:49:36 +0900

    add sector level node

--- sector_app/views.py
+++ sector_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from django.db.models.functions import Length
 from django.utils import timezone
 import csv
 
@@ -14,7 +15,12 @@ def listviewdisplaydata(request):
     return render(request, "Index.html", {'Ratings': results})
 
 def orgdashboard(request):
-    results = Sector.objects.all();
+    results = {}
+    results["level_1"] = Sector.objects.annotate(length=Length('code')).filter(length=2)
+    results["level_2"] = Sector.objects.annotate(length=Length('code')).filter(length=3)
+    results["level_3"] = Sector.objects.annotate(length=Length('code')).filter(length=4)
+    results["level_4"] = Sector.objects.annotate(length=Length('code')).filter(length=5)
+    results["level_5"] = Sector.objects.annotate(length=Length('code')).filter(length=6)
     return render(request, "orgdashboard.html", {'Sectors': results})
 
 def sectororogs(request, sector_id):
@@ -28,7 +34,6 @@ def sectororogdebts(request, sector_id, org_name):
     return render(request, "Index.html", {'Ratings': results})
 
 def importsector(request):
-    
     with open('D:/Django/import.txt', 'r') as fp:
         sectors = csv.reader(fp, delimiter='|')
         row = 0
@@ -50,4 +55,3 @@ def importsector(request):
         fp.close()
     results = Ratings.objects.filter( rating_debt__debt_org__org_sector__id=sector_id )
     return render(request, "Index.html", {'Ratings': results})
-        
